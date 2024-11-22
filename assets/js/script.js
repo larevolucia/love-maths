2dc886230ffc1b604a9fabf49044d8a3f144bb9a
larevolucia <reis.lucia@gmail.com>
2024-11-22 15:28:23 +0100

    Set up structure for game functions

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+// A fun and interactive math quiz game to test and improve arithmetic skills with real-time feedback!
+
+
+function runGame(){
+    
+}
+function checkAnswer(){
+
+}
+function calculateCorrectAnswer(){
+
+}
+function incrementScore(){
+
+}
+function incrementWrongAnswer(){
+
+}
+function displayAdditionQuestion(){
+    
+}
+function displaySubtractionQuestion(){
+
+}
+function displayMultiplyQuestion(){
+
+}
+function displayDivideQuestion(){
+
+}
