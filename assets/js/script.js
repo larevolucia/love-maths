62b6bfcda3f15c09263eac4978698ccf60fdd991
larevolucia <reis.lucia@gmail.com>
2024-11-22 15:42:08 +0100

    Added coded to generate random numbers for the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,30 @@
-// A fun and interactive math quiz game to test and improve arithmetic skills with real-time feedback!
+/**
+ *  A fun and interactive math quiz game to test and improve arithmetic skills with real-time feedback! 
+ * */ 
 
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked submit");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
 
 function runGame(){
-    
+
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 }
 function checkAnswer(){
 
