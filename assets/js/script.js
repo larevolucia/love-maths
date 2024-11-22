a436deff51fb380ad84d3c20254b22255e19b942
larevolucia <reis.lucia@gmail.com>
2024-11-22 15:52:16 +0100

    Display addition question to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 /**
- *  A fun and interactive math quiz game to test and improve arithmetic skills with real-time feedback! 
+ *  A fun and interactive math quiz game to test 
+ *  and improve arithmetic skills with real-time feedback! 
  * */ 
 
 // Wait for the DOM to finish loading before running the game
@@ -14,17 +15,29 @@ document.addEventListener("DOMContentLoaded", function () {
                 alert("You clicked submit");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+    runGame("addition");
 })
 
-function runGame(){
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user answer has been processed
+ */
+function runGame(gameType){
 
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 function checkAnswer(){
 
@@ -38,7 +51,13 @@ function incrementScore(){
 function incrementWrongAnswer(){
 
 }
-function displayAdditionQuestion(){
+/**
+ * Displays the math challenge for addition 
+ */
+function displayAdditionQuestion(operand1, operand2){
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = `+`;
     
 }
 function displaySubtractionQuestion(){
